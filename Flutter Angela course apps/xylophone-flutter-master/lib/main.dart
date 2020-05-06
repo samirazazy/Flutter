17573c2acc6946371cc83bff17257de70dfd25a9
samirazazy <samiralazazy@gmail.com>
2020-05-06 23:43:25 +0200

    xylophone-flutter-master

---
+++ Flutter Angela course apps/xylophone-flutter-master/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Container(),
+        ),
+      ),
+    );
+  }
+}
